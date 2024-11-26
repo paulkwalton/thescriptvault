ad874cfd1ea8b307a00810867df00624c84bac59
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2024-11-26 11:40:33 +0000

    Create enumeratead.sh

---
+++ enumeration/infra/ad/enumeratead.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Set target IP address
+target="192.168.100.100"
+
+# Set credentials
+username="john.doe"
+password="Password1234"
+
+# Function to enumerate group members
+enumerate_group() {
+  group=$1
+  echo "Enumerating members of group: $group"
+  nxc ldap $target -u "$username" -p "$password" -M group-mem -o GROUP="$group"
+  echo ""
+}
+
+# Enumerate Tier 0 groups
+enumerate_group "Enterprise Admins"
+enumerate_group "Schema Admins"
+enumerate_group "Domain Admins"
+
+# Enumerate Tier 1 groups
+enumerate_group "Administrators"
+enumerate_group "Account Operators"
+enumerate_group "Backup Operators"
+enumerate_group "Print Operators"
+enumerate_group "Server Operators"
+enumerate_group "Group Policy Creator Owners"
+
+# Enumerate other important groups
+enumerate_group "Denied RODC Password Replication Group"
+enumerate_group "DnsAdmins"
+
+echo "List Domain Controllers"
+nxc ldap $target -u "$username" -p "$password" --dc-list
+
+echo "Enumerate Domain Trusts"
+nxc ldap $target -u "$username" -p "$password" M enum_trusts
+
+
+echo "Computers with the flag trusted for delegation"
+nxc ldap $target -u "$username" -p "$password" --trusted-for-delegation
+
+echo "Machine Account Quota"
+nxc ldap $target -u "$username" -p "$password" -M maq
+
+echo "Check User Descriptions for Passwords"
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=pass
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=temp
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=del
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=cred
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=admin
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=log
+nxc ldap $target -u "$username" -p "$password" -M get-desc-users -o FILTER=serv
+
+echo "Scan Domain Controller Sysvol and Netlogon for interesting files"
+nxc ldap $target -u "$username" -p "$password" -M spider_plus
+
+
+echo "Run Bloodhound Ingester"
+nxc ldap $target -u "$username" -p "$password" --bloodhound --collection ALL
+
+
+
+ 
+
+
+
