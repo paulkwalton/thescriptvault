83b8851338595eaed31a64b4fde4b127bb471938
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2025-01-01 12:30:29 +0000

    Update kali.sh

--- build/kali.sh
+++ build/kali.sh
@@ -215,6 +215,14 @@ sudo make
 echo "Installing Python packages..."
 sudo pip install mitm6 pyftpdlib Cython python-libpcap
 
+# Install Ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
+
 # Final steps
 echo "Changing default SSH keys..."
 sudo mkdir /etc/ssh/old_keys
