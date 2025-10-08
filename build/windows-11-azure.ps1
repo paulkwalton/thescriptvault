760296602af4b0d22f088c81e37a7fd79d03f1eb
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2025-10-08 15:33:36 +0100

    Updated windows-11-azure.ps1 with refactored script

--- build/windows-11-azure.ps1
+++ build/windows-11-azure.ps1
@@ -1,8 +1,45 @@
 # Refactored Windows Setup Script
 # ---------------------------------------------
 
-
-
+function Remove-UnwantedApps {
+    Write-Host "`n[+] Removing unwanted Windows apps..." -ForegroundColor Cyan
+    $bloatwareApps = @(
+        "Microsoft.ZuneMusic",
+        "Microsoft.ZuneVideo",
+        "Microsoft.WindowsMaps",
+        "Microsoft.MicrosoftSolitaireCollection",
+        "Microsoft.BingWeather",
+        "Microsoft.WindowsAlarms",
+        "Microsoft.WindowsCamera",
+        "Microsoft.GetHelp",
+        "Microsoft.Getstarted",
+        "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.Microsoft3DViewer",
+        "Microsoft.XboxApp",
+        "Microsoft.XboxGameOverlay",
+        "Microsoft.XboxGamingOverlay",
+        "Microsoft.XboxIdentityProvider",
+        "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.MixedReality.Portal",
+        "Microsoft.People",
+        "Microsoft.SkypeApp",
+        "Microsoft.MicrosoftStickyNotes",
+        "Microsoft.YourPhone",
+        "Microsoft.OneConnect",
+        "Microsoft.Todos"
+    )
+    foreach ($app in $bloatwareApps) {
+        try {
+            Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -ErrorAction SilentlyContinue
+            Get-AppxProvisionedPackage -Online | Where-Object DisplayName -EQ $app | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+            Write-Host "[OK] Removed $app" -ForegroundColor Green
+        }
+        catch {
+            Write-Host "[X] Failed to remove $app: $($_.Exception.Message)" -ForegroundColor Red
+        }
+    }
+    Write-Host "[+] Bloatware removal complete." -ForegroundColor Yellow
+}
 
 function Disable-Firewall-And-Configure-System {
     Write-Host "`n[+] Disabling firewalls and configuring system settings..." -ForegroundColor Cyan
@@ -17,10 +54,9 @@ function Disable-Firewall-And-Configure-System {
     }
 }
 
-
-
 # Main Script Execution
 Test-Admin
+Remove-UnwantedApps
 Disable-Firewall-And-Configure-System
 winget install -e --id Iterate.Cyberduck
 winget install -e --id Tenable.Nessus
@@ -30,7 +66,6 @@ winget install -e --id WiresharkFoundation.Wireshark
 winget install -e --id Docker.DockerDesktop
 winget install -e --id Git.Git
 winget install -e --id=Microsoft.Sysinternals.Suite
-
 
 winget install -e --id Microsoft.AzureDataStudio
 winget install -e --id Microsoft.Azure.StorageExplorer
