08f13b0b9ad6f665471ea73516f0d37dde08cd77
paulkwalton <43542501+paulkwalton@users.noreply.github.com>
2025-10-15 14:28:15 +0100

    Add RealVNC VNC Viewer installation command

--- build/windows-11-azure.ps1
+++ build/windows-11-azure.ps1
@@ -282,7 +282,7 @@ winget install -e --id Microsoft.Azure.AZCopy.10 --accept-package-agreements --a
 winget install -e --id Microsoft.OpenJDK.21 --accept-package-agreements --accept-source-agreements
 winget install -e --id Microsoft.Sysinternals.BGInfo --accept-package-agreements --accept-source-agreements
 winget install -e --id Putty.Putty --accept-package-agreements --accept-source-agreements
-
+winget install -e --id RealVNC.VNCViewer --accept-package-agreements --accept-source-agreements
 
 # Add exception to Windows Defender for C:\tools\ BEFORE downloading anything there
 Write-Host "`n[+] Adding Windows Defender exclusion for C:\tools\ ..." -ForegroundColor Cyan
